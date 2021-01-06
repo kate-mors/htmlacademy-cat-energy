31d6f238eba0324988b4d49cd70231990be126c5
Ekaterina <ekaterina.morsino@gmail.com>
2021-01-06 11:03:42 +0100

    Slider attempt

---
+++ source/js/slider.js
@@ -0,0 +1,7 @@
+var sliderList = document.querySelector('.slider__list');
+var sliderItem = document.querySelector('.slider__item');
+var sliderImage = document.querySelector('.slider__image');
+
+sliderList.classList.add('slider__list--js');
+sliderItem.classList.add('slider__item--js');
+sliderImage.classList.add('slider__image--js');
