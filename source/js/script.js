69dd0005185a39bba8ed916033e6b6c2381b8dc6
Ekaterina <ekaterina.morsino@gmail.com>
2021-02-24 16:03:45 +0100

    Adding yandex.map API

--- source/js/script.js
+++ source/js/script.js
@@ -17,7 +17,6 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-
 var sliderItemBefore = document.querySelector('.slider__item--before');
 var sliderItemAfter = document.querySelector('.slider__item--after');
 var sliderBeforeButton = document.querySelector('.slider__button--before');
@@ -45,3 +44,47 @@ sliderBeforeButton.addEventListener('click', function() {
     sliderRange.classList.remove('slider__range--after');
   }
 });
+
+const mapBlock = document.querySelector('#map');
+const breakpointMd = window.matchMedia('(min-width:768px)');
+const breakpointLg = window.matchMedia('(min-width:1440px)');
+
+const mapState = {
+  latitude: 59.938635,
+  longitude: 30.323118,
+  iconWidth: 56,
+  iconHeight: 52
+};
+
+if (breakpointMd.matches) {
+  mapState.iconWidth = 113;
+  mapState.iconHeight = 106;
+}
+
+if (breakpointLg.matches) {
+  mapState.longitude =  30.320546;
+}
+
+if (map) {
+  ymaps.ready(init);
+  function init() {
+    const myMap = new ymaps.Map(map, {
+      center: [mapState.latitude, mapState.longitude],
+      zoom: 17,
+    });
+    const myPlacemark = new ymaps.Placemark(
+      [59.938635, 30.323118],
+      {
+        hintContent: 'ул. Большая Конюшенная, д. 19/8',
+      },
+      {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/svg/map-pin.svg',
+        iconImageSize: [mapState.iconWidth, mapState.iconHeight],
+        iconImageOffset: [-mapState.iconWidth / 2, -mapState.iconHeight],
+      },
+    );
+
+    myMap.geoObjects.add(myPlacemark);
+  }
+}
